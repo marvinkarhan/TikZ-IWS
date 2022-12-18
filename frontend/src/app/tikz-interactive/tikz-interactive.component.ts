6e728916d7c6cb4cde68031fcfd0596bba9f828b
Marvin Karhan <1821400@stud.hs-mannheim.de>
2022-12-18 22:53:45 +0100

    Add error messages

--- frontend/src/app/tikz-interactive/tikz-interactive.component.ts
+++ frontend/src/app/tikz-interactive/tikz-interactive.component.ts
@@ -1,42 +1,58 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
+import { BehaviorSubject, fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { TikzService } from './tikz.service';
 
 @Component({
   selector: 'app-tikz-interactive',
   templateUrl: './tikz-interactive.component.html',
-  styleUrls: ['./tikz-interactive.component.scss']
+  styleUrls: ['./tikz-interactive.component.scss'],
 })
 export class TikzInteractiveComponent implements AfterViewInit {
+  public textarea!: HTMLTextAreaElement | null;
+  public output!: HTMLElement | null;
+  public errorMessage = new BehaviorSubject<string>('');
+  private _texOutput = '';
+  private errorRegex = /(?<=\*\*entering extended mode).*(?=\? Type <return> to proceed)/ms;
 
-  textarea!: HTMLTextAreaElement | null;
-  output!: HTMLElement | null;
+  @ViewChild('outputContainer') outputContainer!: ElementRef;
 
-  constructor(private readonly _tikzService: TikzService) { }
+  constructor(private readonly _tikzService: TikzService, private _cd: ChangeDetectorRef) {}
 
   ngAfterViewInit(): void {
     this.textarea = document.getElementById('tikz-code') as HTMLTextAreaElement;
     this.output = document.getElementById('output-container');
+    const log = console.log;
+    console.log = (str) => {
+      this.parseError(str);
+      log(str);
+    };
+    fromEvent(this.textarea, 'keyup')
+      .pipe(debounceTime(300))
+      .subscribe(() => this.update());
+  }
 
-    this.textarea?.addEventListener('keyup', () => {
-      this.debounce(() => this.update())();
-    });
+  parseError(log: string) {
+    this._texOutput += log + '\n';
+    this.errorMessage.next('');
+    const hasError =
+      this._texOutput.includes('Emergency stop') &&
+      this.textarea &&
+      this.textarea.value.length > 0;
+    if (hasError) {
+      this.errorMessage.next(this._texOutput.match(this.errorRegex)?.[0] || '');
+    }
+    this._cd.detectChanges();
   }
 
   update() {
     if (!this.output || !this.textarea) return;
+    this._texOutput = '';
     const s = document.createElement('script');
     s.setAttribute('type', 'text/tikz');
     s.textContent = `${this.textarea.value}`;
     this.output.innerHTML = '';
     this.output.appendChild(s);
     this._tikzService.process_tikz(s);
-  }
-
-  debounce(fn: () => any, delay = 300) {
-    let timeoutID: any;
-    return () => {
-      if (timeoutID) clearTimeout(timeoutID);
-      timeoutID = setTimeout(() => fn(), delay);
-    };
   }
 }
